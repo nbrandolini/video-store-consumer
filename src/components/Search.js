843ee72d9608ca74527afef9e318a8215d5b4831
marikoja <Marikoanderson@gmail.com>
2018-06-18 15:40:21 -0700

    added Search component and installed axios

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+class Search extends Component {
+  render() {
+    return (
+      <section>
+        <p> Please fucking show on the page</p>
+      </section>
+    )
+  }
+
+}
+
+export default Search;
